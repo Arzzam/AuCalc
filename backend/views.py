1a48bdae0005c93687fd28c6b211f3d3451d0ce9
NanthaKumar <nknanthakumar13@gmail.com>
2022-08-31 19:08:55 +0530

    Added degree view

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
     return HttpResponse("<h1>Backend Running...</h1>")
+
+
+def degrees(request):
+    data = [
+        "Bachelor of Engineering",
+        "Bachelor of Technology"
+    ]
+    return JsonResponse(data)
