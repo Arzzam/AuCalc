0ace52b9e552dcabf459c59322639befca8b57da
NanthaKumar <nknanthakumar13@gmail.com>
2022-08-31 20:56:13 +0530

    Added example of api calling in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState({});
+
+    function fetchData(event) {
+        
+        axios.get('/api/degrees')
+        .then((response) => {
+            setData(response.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+        event.preventDefault();
+    }
+
+    return (
+        <div style={{textAlign: 'center', 'marginTop': '50px'}}>
+            <p>{JSON.stringify(data)}</p>
+            <button onClick={fetchData}>Get Data</button>
+        </div>
+    );
 }
 
 export default App;
