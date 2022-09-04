743fdb457d788780ecfb5f94d457a0a77faf152b
NanthaKumar <nknanthakumar13@gmail.com>
2022-09-04 23:06:25 +0530

    Updated urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
 from .views import *
 
 urlpatterns = [
-   path('degrees/', api.degrees, name='degrees')
+    path('regulations/', regulation_list),
+    path('degrees/', degree_list),
+    path('departments/', department_list),
+    path('semesters/', semester_list),
+    path('subjects/', semester_subject_list),
 ]
