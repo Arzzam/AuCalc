e21b95c325518c2759577e049fa8dd629983a276
NanthaKumar <nknanthakumar13@gmail.com>
2022-09-04 22:50:27 +0530

    Revamped data models

--- backend/models.py
+++ backend/models.py
@@ -1,68 +1,73 @@
 from django.db import models
 
 
-# Create your models here.
-class Degree(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Regulation(models.Model):
     year = models.IntegerField(unique=True)
 
-    def __str__(self):
-        return str(self.year)
 
-
-class Department(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Course(models.Model):
-    code = models.CharField(max_length=50, unique=True)
-    title = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return f"{self.code} - {self.title}"
+class Degree(models.Model):
+    name = models.CharField(max_length=50)
+    regulation = models.ForeignKey(Regulation, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['code', 'title'],
-                                    name='unique_course_title')
+            models.UniqueConstraint(
+                fields=[
+                    'name',
+                    'regulation'
+                ],
+                name='unique_name_regulation'
+            )
+        ]
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    degree = models.ForeignKey(Degree, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=[
+                    'name',
+                    'degree'
+                ],
+                name='unique_name_degree'
+            )
+        ]
+
+
+class Course(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=[
+                    'code',
+                    'title'
+                ],
+                name='unique_code_title'
+            )
         ]
 
 
 class SemesterSubject(models.Model):
-    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     credits = models.IntegerField()
-    department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
     semester = models.IntegerField()
-    regulation_id = models.ForeignKey(Regulation, on_delete=models.CASCADE)
-    degree_id = models.ForeignKey(Degree, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return " - ".join(
-            (str(obj) for obj in (
-                self.course_id.code,
-                self.semester,
-                self.department_id,
-                self.regulation_id
-            ))
-        )
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=[
-                'course_id',
-                'credits',
-                'department_id',
-                'semester',
-                'regulation_id',
-                'degree_id'
-            ], name='unique_semester_subject_fields')
+            models.UniqueConstraint(
+                fields=[
+                    'course',
+                    'credits',
+                    'semester',
+                    'department'
+                ],
+                name='unique_subject_credit_department'
+            )
         ]
