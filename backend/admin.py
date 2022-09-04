257dc6fb7d0c9564a8353755ef9f38c384d41d2e
NanthaKumar <nknanthakumar13@gmail.com>
2022-09-04 22:55:32 +0530

    Removed models in admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,11 +1,1 @@
 from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register([
-    SemesterSubject,
-    Degree,
-    Regulation,
-    Department,
-    Course
-])
