e6ae59580256c6d6d5811872ede15dd978ee063b
NanthaKumar <nknanthakumar13@gmail.com>
2022-09-17 15:06:33 +0530

    Updated route /gpa from Gpa to Gpa2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import About from "./Pages/About";
 import Cgpa from "./Pages/Cgpa";
 import Gpa from "./Pages/Gpa";
+import Gpa2 from "./Pages/Gpa2";
 import Home from "./Pages/Home";
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/gpa" element={<Gpa />} />
+        <Route exact path="/gpa" element={<Gpa2 />} />
         <Route exact path="/cgpa" element={<Cgpa />} />
         <Route exact path="/about" element={<About />} />
       </Routes>
