43144c466d58f3c47a80b023ad7945921ad6ccda
NanthaKumar <nknanthakumar13@gmail.com>
2022-09-21 20:19:11 +0530

    Revamped entire component

--- frontend/src/Components/Progress/Progress.jsx
+++ frontend/src/Components/Progress/Progress.jsx
@@ -1,9 +1,18 @@
-function Progress() {
+export default function Progress(props) {
     return (
-        <svg className="animate-spin-slow h-32 w-32 m-0 text-red-600 overflow-hidden inline" viewBox="0 0 24 24" fill="none">
-            <circle className="animate-spin-inner" stroke="currentColor" cx="12" cy="12" r="10" fill="none" stroke-width="3"></circle>
+        <svg
+            className={`animate-spin-slow m-0 overflow-hidden inline ${props.className}`}
+            viewBox="0 0 24 24"
+            fill="none">
+            <circle
+                className="animate-spin-inner"
+                stroke="currentColor"
+                cx="12"
+                cy="12"
+                r="10"
+                fill="none"
+                strokeWidth="2"
+            ></circle>
         </svg>
     );
 }
-
-export default Progress;
