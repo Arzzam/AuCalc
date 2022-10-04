5af55af086a850c728d771df766ee8386a89d651
NanthaKumar <nknanthakumar13@gmail.com>
2022-10-04 18:18:16 +0530

    Add background color inheritance

--- frontend/src/Components/Input.jsx
+++ frontend/src/Components/Input.jsx
@@ -2,7 +2,7 @@ export default function Input(props) {
     return (
         <input
             {...props}
-            className={`px-2 py-1.5 w-fit border border-solid border-slate-300 rounded disabled:opacity-80 ${props.className}`}
+            className={`px-2 py-1.5 outline-none w-fit bg-inherit border border-solid border-slate-300 focus:border-slate-500 hover:border-slate-500 rounded disabled:opacity-80 ${props.className}`}
         />
     );
 }
