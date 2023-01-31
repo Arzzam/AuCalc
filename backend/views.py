8be28ff7e46311410cf722e9f2eb04521b7caaeb
NanthaKumar <nknanthakumar13@gmail.com>
2023-01-31 22:34:23 +0530

    Update views with api decorator

--- backend/views.py
+++ backend/views.py
@@ -1,69 +1,66 @@
 from django.http.response import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
 from django.db.models import Sum
 
 from backend.serializer import *
 
 
-def index(request):
-    return HttpResponse("<h1>Backend Running...</h1>")
-
-
+@api_view(['GET'])
 def regulation_list(request):
-    if request.method == 'GET':
-        regulations = Regulation.objects.all()
-        serializer = RegulationSerializer(regulations, many=True)
-        return JsonResponse(serializer.data, safe=False)
+    regulations = Regulation.objects.all()
+    serializer = RegulationSerializer(regulations, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
 
-@csrf_exempt
+@api_view(['POST'])
 def degree_list(request):
-    if request.method == "POST":
-        data = JSONParser().parse(request)
+    data = JSONParser().parse(request)
 
-        degrees = Degree.objects.filter(regulation=data['regulation_id'])
-        if not degrees:
-            return HttpResponse(status=404)
+    degrees = Degree.objects.filter(regulation=data['regulation_id'])
 
-        serializer = DegreeSerializer(degrees, many=True)
-        return JsonResponse(serializer.data, safe=False)
+    if not degrees:
+        return HttpResponse(status=404)
+
+    serializer = DegreeSerializer(degrees, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
 
-@csrf_exempt
+@api_view(['POST'])
 def department_list(request):
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        departments = Department.objects.filter(degree=data['degree_id'])
-        if not departments:
-            return HttpResponse(status=404)
+    data = JSONParser().parse(request)
+    departments = Department.objects.filter(degree=data['degree_id'])
 
-        serializer = DepartmentSerializer(departments, many=True)
-        return JsonResponse(serializer.data, safe=False)
+    if not departments:
+        return HttpResponse(status=404)
+
+    serializer = DepartmentSerializer(departments, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
 
-@csrf_exempt
+@api_view(['POST'])
 def semester_list(request):
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        semesters = SemesterSubject.objects.filter(
-            department=data['department_id']
-        ).values('semester').distinct().count()
-        if not semesters:
-            return HttpResponse(status=404)
+    data = JSONParser().parse(request)
+    semesters = SemesterSubject.objects.filter(
+        department=data['department_id']
+    ).values('semester').distinct().count()
 
-        data = [{'id': i, 'data': i} for i in range(1, semesters + 1)]
-        return JsonResponse(data, safe=False)
+    if not semesters:
+        return HttpResponse(status=404)
+
+    data = [{'id': i, 'data': i} for i in range(1, semesters + 1)]
+    return JsonResponse(data, safe=False)
 
 
-@csrf_exempt
+@api_view(['POST'])
 def semester_subject_list(request):
-    if request.method == 'POST':
         data = JSONParser().parse(request)
         semester_subs = SemesterSubject.objects.filter(
             department=data['department_id'],
             semester=data['semester_id']
         )
+
         if not semester_subs:
             return HttpResponse(status=404)
 
@@ -71,9 +68,8 @@ def semester_subject_list(request):
         return JsonResponse(serializer.data, safe=False)
 
 
-@csrf_exempt
+@api_view(['POST'])
 def semester_credit_list(request):
-    if request.method == 'POST':
         data = JSONParser().parse(request)
         semester_credits = SemesterSubject.objects \
             .filter(department=data['department_id']) \
